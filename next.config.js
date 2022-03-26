343db7e7dfe8b5ce0f6391dd0ded47e89ae0a635
Jemal <jebdi12@gmail.com>
2022-03-26 09:46:13 -0500

    Add CDN

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? "https://cdn.statically.io/gh/MD-Transportation/md-transportation.github.io/gh-pages/" : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
