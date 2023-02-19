78a964e921e5d94087fd9450662fe4fa082b6f60
Jemal Abdullahi <jebdi12@gmail.com>
2023-02-19 14:58:20 -0600

    services update

--- src/sections/services.js
+++ src/sections/services.js
@@ -1,6 +1,6 @@
 /** @jsxImportSource theme-ui */
 
-import { Image } from "theme-ui";
+import { Box, Heading, Image } from "theme-ui";
 import Carousel from "components/carousel";
 
 const carouselImages = [
@@ -11,88 +11,135 @@ const carouselImages = [
   { src: "/side_bus.jpg", alt: "Side bus" },
 ];
 
+const services = { 
+    row1: [
+        {
+          icon: "/security-camera.svg",
+          header: "Companion Care/Services",
+          description: `Non-medical support services designed to provide social 
+                        interaction, companionship, and assistance with daily living
+                        activities.`
+        },
+        {
+          icon: "/security-camera.svg",
+          header: "Homemaker Services",
+          description: `Home management services, including meal preparation, cleaning,
+                        and other household chores, to assist individuals with daily
+                        living activities.`
+        },
+        {
+          icon: "/security-camera.svg",
+          header: "Night Supervision",
+          description: `Over-night monitoring and supervision services to ensure the
+                        safety and well-being of individuals during the night hours.`
+        }
+    ],
+    row2: [
+        {
+          icon: "/security-camera.svg",
+          header: "Independent Living Skills",
+          description: `Training and support services to help individuals with
+                        disabilities to develop skills and confidence to live independently.`
+        },
+        {
+          icon: "/security-camera.svg",
+          header: "24 Hour Emergency Assistance",
+          description: `Access to immediate assistance 24 hours a day, 7 days a week
+                        in case of an emergency.`
+        },
+        {
+          icon: "/security-camera.svg",
+          header: "Individual Community Living Support",
+          description: `Support services to help individuals participate in community
+                        activities and events, promoting independence and integration
+                        into the community.`
+        },
+    ],
+    row3: [
+        {
+          icon: "/security-camera.svg",
+          header: "Respite Care Services",
+          description: `Respite care services are temporary in-home care services
+                        provided to individuals who need a break from their caregiving
+                        responsibilities. These services can be a lifesaver for family
+                        caregivers, who may feel overwhelmed or burnt out, and can
+                        provide them with the time and space they need to recharge.`
+        }
+    ],
+};
+
+
 export default function ServicesSection() {
   return (
     <section sx={styles.services}>
-      <div className="container py-5">
-        <div className="row align-items-center">
-          <div
-            className="position-relative col-12 col-lg-6 order-last order-lg-first mt-5 mt-lg-0"
-            sx={styles.services.carousel}
-          >
-            <Carousel slides={carouselImages} />
-          </div>
-          <div className="col-12 col-lg-6 py-5">
-            <div className="row">
-              <div className="col-12 col-lg-8 mx-auto">
-                <span className="badge rounded-pill" sx={{ background: "primary" }}>
-                  Request a Vehicle
-                </span>
-                <h2 className="mt-3 mb-5 fs-1 fw-bold">Key Features</h2>
-                <div className="d-flex mb-4 pb-1">
-                  <span className="me-4 text-primary" sx={styles.services.icon}>
-                    <Image src={"/security-camera.svg"} alt="bus" />
-                  </span>
-                  <div>
-                    <h5>Accountable-Reliable-Safe</h5>
-                    <p className="mb-0 text-muted lh-lg">
-                      All MD vehicles are equipped with 2-way radios and internal cameras to ensure accountability,
-                      reliability, and safety. All drivers carry cell phones as a backup communication device.
-                    </p>
-                  </div>
+        <Box sx={styles.services.headerBox}>
+          <Heading as={"h1"} variant="heroPrimary" sx={styles.services.header}>
+            <span sx={{ color: "primary" }}>Key Services</span>
+          </Heading>
+        </Box>
+      <div className="container">
+          {
+              Object.keys(services).map((row) => 
+                <div className="row">
+                  {
+                      services[row].map((item) => 
+                        <div className="col mx-auto">
+                          <div className="mb-4 pb-1 mx-auto">
+                            <div className="d-flex justify-content-center mx-auto" sx={styles.services.iconWrapper}>
+                              <div className="me-4 text-primary" sx={styles.services.iconWrapper.icon}>
+                                <Image src={item.icon} alt="bus" />
+                              </div>
+                            </div>
+                            <div>
+                              <h5>{item.header}</h5>
+                              <p className="mb-0 text-muted lh-lg">{item.description}</p>
+                            </div>
+                          </div>
+                        </div>
+                      )
+                  }
                 </div>
-                <div className="d-flex mb-4 pb-1">
-                  <span className="me-4 text-primary">
-                    <svg
-                      width="24px"
-                      height="24px"
-                      viewBox="-1.5 -1 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                      preserveAspectRatio="xMinYMin"
-                      className="jam jam-gps"
-                    >
-                      <path
-                        sx={{ fill: "#F5A400" }}
-                        d="M18.913 2.9L2.632 9.226l4.829 2.006a5.767 5.767 0 0 1 3.118 3.119l2.006 4.828L18.913 2.9zm1.847.682l-6.328 16.281c-.4 1.03-1.551 1.557-2.571 1.18a1.923 1.923 0 0 1-1.11-1.067l-2.007-4.83a3.845 3.845 0 0 0-2.079-2.078l-4.828-2.006C.833 10.645.375 9.486.814 8.472A2.05 2.05 0 0 1 1.949 7.38L18.23 1.052a1.945 1.945 0 0 1 2.53 2.53z"
-                      ></path>
-                    </svg>
-                  </span>
-                  <div>
-                    <h5>GPS System</h5>
-                    <p className="mb-0 text-muted lh-lg">
-                      Our routes are tracked by GPS to ensure completion of services.
-                    </p>
-                  </div>
-                </div>
-                <div className="d-flex">
-                  <span className="me-4 text-primary" sx={styles.services.icon}>
-                    <Image src={"/school-bus.svg"} alt="bus" />
-                  </span>
-                  <div>
-                    <h5>Vehicles</h5>
-                    <p className="mb-0 text-muted lh-lg">
-                      Our vehicles are safe, clean, and comfortable. Also, not only do we have large capacity school
-                      buses, we have 7-passenger vans as well!
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+              )
+          }
       </div>
     </section>
   );
 }
 
+          /*<div
+            className="position-relative col-12 col-lg-6 order-last order-lg-first mt-5 mt-lg-0"
+            sx={styles.services.carousel}
+          >
+            <Carousel slides={carouselImages} />
+    pt: ["140px", "145px", "155px", "170px", null, null, "180px", "215px"],
+          </div>*/
 const styles = {
   services: {
     pt: ["140px", "145px", "155px", "170px", null, null, "180px", "215px"],
     pb: [2, null, 0, null, 2, 0, null, 5],
     height: "100%",
     width: "100%",
-    icon: {
-      width: "100px",
+    headerBox: {
+      width: ["100%", "90%", "535px", null, "57%", "60%", "68%", "60%"],
+      mx: "auto",
+      textAlign: "center",
+      mb: ["40px", null, null, null, null, 7],
+    },
+    header: {
+      "&::after": {
+        display: "block",
+        content: '""',
+        width: "50%",
+        margin: "0 auto",
+        borderBottom: "2px solid",
+        borderBottomColor: "primary",
+      },
+    },
+    iconWrapper: {
+      width: "100%",
+        icon: {
+          width: "100px",
+        }
     },
     carousel: {
       "@media screen and (max-width: 400px)": {
